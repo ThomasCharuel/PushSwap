4be2878fe09f7047feebefe75c59d6bd7f50690f
tcharuel <tcharuel@student.42.fr>
2023-12-04 11:22:05 +0100

    Norminette

--- move_analysis.c
+++ move_analysis.c
@@ -6,7 +6,7 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 18:14:37 by tcharuel          #+#    #+#             */
-/*   Updated: 2023/12/04 10:58:42 by tcharuel         ###   ########.fr       */
+/*   Updated: 2023/12/04 11:21:09 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,28 @@ void	set_rotations_count(t_stack_node *node, bool is_stack_a)
 	}
 }
 
-void	set_node_moves(t_stack_node *node, int ra_count, int rb_count,
-		int rra_count)
+void	set_node_moves(t_stack_node *node, int cost_with_rr, int cost_with_rrr)
 {
-	node->ra_count = ra_count;
-	node->rb_count = rb_count;
-	node->rra_count = rra_count;
+	if (cost_with_rr == node->move_cost)
+	{
+		node->rra_count = 0;
+		node->rrb_count = 0;
+	}
+	else if (cost_with_rrr == node->move_cost)
+	{
+		node->ra_count = 0;
+		node->rb_count = 0;
+	}
+	else if (node->ra_count + node->rrb_count == node->move_cost)
+	{
+		node->rb_count = 0;
+		node->rra_count = 0;
+	}
+	else
+	{
+		node->ra_count = 0;
+		node->rrb_count = 0;
+	}
 }
 
 void	set_moves_cost(t_stack_node *stack, bool is_stack_a)
@@ -51,17 +67,7 @@ void	set_moves_cost(t_stack_node *stack, bool is_stack_a)
 		stack->move_cost = ft_min(4, cost_with_rr, cost_with_rrr,
 				stack->ra_count + stack->rrb_count, stack->rra_count
 				+ stack->rb_count);
-		if (cost_with_rr == stack->move_cost)
-			set_node_moves(stack, stack->ra_count, stack->rb_count, 0);
-		else if (cost_with_rrr == stack->move_cost)
-			set_node_moves(stack, 0, 0, stack->rra_count);
-		else if (stack->ra_count + stack->rrb_count == stack->move_cost)
-			set_node_moves(stack, stack->ra_count, 0, 0);
-		else
-			set_node_moves(stack, 0, stack->rb_count, stack->rra_count);
-		if (cost_with_rr == stack->move_cost || stack->rra_count
-			+ stack->rb_count)
-			stack->rrb_count = 0;
+		set_node_moves(stack, cost_with_rr, cost_with_rrr);
 		stack = stack->next;
 	}
 }
